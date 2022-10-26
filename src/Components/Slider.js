d454144434599c2a76f3316386c05dbe107d9ee5
dacier97 <danielor6@hotmail.com>
2022-10-25 21:25:34 -0500

    first Version

---
+++ src/Components/Slider.js
@@ -0,0 +1,99 @@
+/* eslint-disable */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable react/jsx-no-comment-textnodes */
+import React from 'react';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
+function ImageSlider() {
+    
+    let settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        cssEase: "linear"
+    }
+    return (
+        
+        <Slider {...settings}>
+            <div className="card-wrapper">
+                <div className="card">
+                    <div className="card-image">
+                        // eslint-disable-next-line jsx-a11y/alt-text
+                        <img src="images/8.jpg" alt="ima" />
+                    </div>
+                    <ul className="social-icons">
+
+                        <li><a href="#"><i className="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i className="fa fa-instagram"></i></a></li>
+                        <li><a href="#"><i className="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i className="fa fa-dribbble"></i></a></li>
+                    </ul>
+                    <div className="details">
+                        <h2>John Doe <span className="job-title">UI Developer</span></h2>
+                    </div>
+                </div>
+            </div>
+            <div className="card-wrapper">
+                <div className="card">
+                    <div className="card-image">
+                        <img src="images/15.jpg" alt="ima"/>
+                    </div>
+                    <ul className="social-icons">
+                        <li><a href="#"><i className="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i className="fa fa-instagram"></i></a></li>
+                        <li><a href="#"><i className="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i className="fa fa-dribbble"></i></a></li>
+                    </ul>
+                    <div className="details">
+                        <h2>John Doe <span className="job-title">UI Developer</span></h2>
+                    </div>
+                </div>
+            </div>
+            <div className="card-wrapper">
+                <div className="card">
+                    <div className="card-image">
+                        <img src="images/7.jpg" alt="ima" />
+                    </div>
+                    <ul className="social-icons">
+                        <li><a href="#"><i className="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i className="fa fa-instagram"></i></a></li>
+                        <li><a href="#"><i className="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i className="fa fa-dribbble"></i></a></li>
+                    </ul>
+                    <div className="details">
+                        <h2>John Doe <span className="job-title">UI Developer</span></h2>
+                    </div>
+                </div>
+            </div>
+            <div className="card-wrapper">
+                <div className="card">
+                    <div className="card-image">
+                        <img src="images/16.jpg" alt="ima"/>
+                    </div>
+                    <ul className="social-icons">
+                        <li><a href="#"><i className="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i className="fa fa-instagram"></i></a></li>
+                        <li><a href="#"><i className="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i className="fa fa-dribbble"></i></a></li>
+                    </ul>
+                    <div className="details">
+                        <h2>John Doe <span className="job-title">UI Developer</span></h2>
+                    </div>
+                </div>
+            </div>
+        </Slider>
+    )
+}
+
+export default ImageSlider
+
+//... is called spread operator
+
+//thanks for watching
+//pleae subscribe my channel
