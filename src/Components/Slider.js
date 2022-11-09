b773f10d0bf282868298263b63921fb4a2cbf0e7
dacier97 <danielor6@hotmail.com>
2022-11-08 20:17:41 -0500

    version 2

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -6,18 +6,62 @@ import React from 'react';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import image1 from'../images/1.jpg';
+import image2 from'../images/2.jpg';
+import image3 from'../images/3.jpg';
+import image4 from'../images/4.jpg';
+import image5 from'../images/5.jpg';
 
 
 function ImageSlider() {
     
-    let settings = {
+    /*let settings = {
         dots: true,
         infinite: true,
         speed: 500,
         slidesToShow: 3,
         slidesToScroll: 1,
         cssEase: "linear"
-    }
+    }*/
+
+    var settings = {
+
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        cssEase: "linear",
+
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true,
+           
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2,
+            
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          }
+        }
+      ]
+    };
     return (
         
         <Slider {...settings}>
@@ -25,7 +69,7 @@ function ImageSlider() {
                 <div className="card">
                     <div className="card-image">
                         // eslint-disable-next-line jsx-a11y/alt-text
-                        <img src="images/8.jpg" alt="ima" />
+                        <img src={image1} alt="ima" />
                     </div>
                     <ul className="social-icons">
 
@@ -35,14 +79,14 @@ function ImageSlider() {
                         <li><a href="#"><i className="fa fa-dribbble"></i></a></li>
                     </ul>
                     <div className="details">
-                        <h2>John Doe <span className="job-title">UI Developer</span></h2>
+                        <h2>Jessica <span className="job-title">UI Developer</span></h2>
                     </div>
                 </div>
             </div>
             <div className="card-wrapper">
                 <div className="card">
                     <div className="card-image">
-                        <img src="images/15.jpg" alt="ima"/>
+                    <img src={image2} alt="ima" />
                     </div>
                     <ul className="social-icons">
                         <li><a href="#"><i className="fa fa-facebook"></i></a></li>
@@ -51,14 +95,14 @@ function ImageSlider() {
                         <li><a href="#"><i className="fa fa-dribbble"></i></a></li>
                     </ul>
                     <div className="details">
-                        <h2>John Doe <span className="job-title">UI Developer</span></h2>
+                        <h2>Danna<span className="job-title">Full Stack Developer</span></h2>
                     </div>
                 </div>
             </div>
             <div className="card-wrapper">
                 <div className="card">
                     <div className="card-image">
-                        <img src="images/7.jpg" alt="ima" />
+                        <img src={image3} alt="ima" />
                     </div>
                     <ul className="social-icons">
                         <li><a href="#"><i className="fa fa-facebook"></i></a></li>
@@ -67,14 +111,14 @@ function ImageSlider() {
                         <li><a href="#"><i className="fa fa-dribbble"></i></a></li>
                     </ul>
                     <div className="details">
-                        <h2>John Doe <span className="job-title">UI Developer</span></h2>
+                        <h2>Linda<span className="job-title">Marketing Design</span></h2>
                     </div>
                 </div>
             </div>
             <div className="card-wrapper">
                 <div className="card">
                     <div className="card-image">
-                        <img src="images/16.jpg" alt="ima"/>
+                    <img src={image4} alt="ima" />
                     </div>
                     <ul className="social-icons">
                         <li><a href="#"><i className="fa fa-facebook"></i></a></li>
@@ -83,7 +127,23 @@ function ImageSlider() {
                         <li><a href="#"><i className="fa fa-dribbble"></i></a></li>
                     </ul>
                     <div className="details">
-                        <h2>John Doe <span className="job-title">UI Developer</span></h2>
+                        <h2>Violeta <span className="job-title">Sales Manager</span></h2>
+                    </div>
+                </div>
+            </div>
+            <div className="card-wrapper">
+                <div className="card">
+                    <div className="card-image">
+                    <img src={image5} alt="ima" />
+                    </div>
+                    <ul className="social-icons">
+                        <li><a href="#"><i className="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i className="fa fa-instagram"></i></a></li>
+                        <li><a href="#"><i className="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i className="fa fa-dribbble"></i></a></li>
+                    </ul>
+                    <div className="details">
+                        <h2>Melissa <span className="job-title">Project Manager</span></h2>
                     </div>
                 </div>
             </div>
@@ -95,5 +155,3 @@ export default ImageSlider
 
 //... is called spread operator
 
-//thanks for watching
-//pleae subscribe my channel
